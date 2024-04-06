0dd488dd381f9ce0caa285413a82426f6cc7a08e
Sergey Vinokurov <serg.foo@gmail.com>
2024-04-06 11:41:29 +0100

    Add set operations-like benchmark for Data.Map.partitionKeys

---
+++ containers-tests/benchmarks/SetOperations/PartitionKeys-MapSet.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+import SetOperations
+import Utils.Containers.Internal.StrictPair
+
+main =
+  benchmark'
+    (\xs -> M.fromList [(x, x) | x <- xs])
+    S.fromList
+    True
+    [ ("partitionKeys", M.partitionKeys)
+    ]
